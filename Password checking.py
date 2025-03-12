04de4aae89f17e4c64a4e507d1fc70bbb4d5cc15
PallapothuNancharamma <pallapothunancharamma@gmail.com>
2025-03-12 21:33:47 +0530

    Update Password checking.py

--- Password checking.py
+++ Password checking.py
@@ -1,19 +1,23 @@
-pass_word=str(input("enter string"))
+pass_word=str(input("enter the password:"))
 b=len(pass_word)
 digit=False
 upper=False
 lower=False
+character=False
+special_character = {'!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'}
 if b<8:
-    print("Ivalid password")
+    print("Invalid password: Password must be at least 8 characters long.")
 for i in pass_word:
     if(i.isupper()):
         upper=True
     elif(i.islower()):
         lower=True    
     elif(i.isdigit()):
-        digit=True 
-if(digit and upper and lower) :
-    print("Valid password")
+        digit=True
+    if i in special_character:
+        character=True
+        
+if(digit and upper and lower and character) :
+        print("Valid password")
 else:
-    print("Invalid password: Password must be at least 8 characters long.")
-    
+    print("Invalid password: Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
