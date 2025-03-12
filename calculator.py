92e062e7f31106d4e6418e2313c75bfd58645f18
PallapothuNancharamma <pallapothunancharamma@gmail.com>
2025-03-12 16:12:10 +0530

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+a=int(input("enter integer:"))
+b=int(input())
+operator=input()
+if operator=="+":
+    print(a+b)
+elif operator=="-":
+    print(a-b)
+elif operator=="*":
+    print(a*b)
+elif operator=="/":
+    print(a/b)
+elif operator=="%":
+    print(a%b)
+elif operator=="//":
+    print(a//b)
+elif operator=="**":
+    print(a**b)
+    
+    
