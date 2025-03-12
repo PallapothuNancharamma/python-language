b2f8c1f869d99f88388e3b0ed72939273ebd6c95
PallapothuNancharamma <pallapothunancharamma@gmail.com>
2025-03-12 16:27:38 +0530

    Month.py

---
+++ Month.py
@@ -0,0 +1,15 @@
+month=int(input("enter month:"))
+if month==1 or month==2:
+    print("cold")
+elif month==3 or  month==4:
+    print("spring")
+elif month==5 or  month==6:
+    print("summer")
+elif month==7 or  month==8:
+    print("rain")
+elif month==9 or  month==10:
+    print("autumn")
+elif month==11 or  month==12:
+    print("winter")
+else:
+    print("invalid month",month)
